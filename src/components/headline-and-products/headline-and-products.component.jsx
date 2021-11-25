46864ceef2d5dca65df0839eea06f09b856df8ab
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-11-25 19:38:34 -0300

    feat: headline products

---
+++ src/components/headline-and-products/headline-and-products.component.jsx
@@ -0,0 +1,49 @@
+import SectionHeadline from '../section-headline/section-headline.component.jsx';
+import ProductCard from '../cards/product-card.component.jsx';
+import Pagination from '../pagination/pagination.component.jsx';
+import ProductCardSkeleton from '../cards/product-card-skeleton.component.jsx';
+
+const HeadlineAndProducts = ({
+  tag,
+  headline,
+  children,
+  loading,
+  products,
+  showPagination,
+  showHeadline,
+  pages,
+  page,
+  setPageNumber,
+}) => {
+  return (
+    <div className='relative bg-blue-gray-50 py-16 sm:py-24 lg:py-32'>
+      <div className='relative pb-12'>
+        {showHeadline && (
+          <SectionHeadline tag={tag} headline={headline}>
+            {children}
+          </SectionHeadline>
+        )}
+        <div className='mt-12 mx-auto max-w-md px-4 grid gap-8 sm:max-w-lg sm:px-6 lg:px-8 lg:grid-cols-3 lg:max-w-7xl'>
+          {loading
+            ? [...Array(3)].map((_, i) => <ProductCardSkeleton key={i} />)
+            : products?.map((product) => (
+                <ProductCard product={product} key={product?._id} />
+              ))}
+        </div>
+      </div>
+      {showPagination && (
+        <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+          <div className='max-w-3xl mx-auto'>
+            <Pagination
+              pages={pages}
+              page={page}
+              setPageNumber={setPageNumber}
+            />
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default HeadlineAndProducts;
---
+++ src/components/section-headline/section-headline.component.jsx
@@ -0,0 +1,17 @@
+const SectionHeadline = ({ tag, headline, children }) => {
+  return (
+    <div className='text-center mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-8 lg:max-w-7xl'>
+      <h2 className='text-base font-semibold tracking-wider text-rose-600 uppercase font-hind'>
+        {tag}
+      </h2>
+      <p className='mt-2 text-3xl font-extrabold text-blue-gray-800 tracking-tight sm:text-4xl font-hind'>
+        {headline}
+      </p>
+      <p className='mt-5 mx-auto max-w-prose text-xl text-blue-gray-500 font-hind'>
+        {children}
+      </p>
+    </div>
+  );
+};
+
+export default SectionHeadline;
