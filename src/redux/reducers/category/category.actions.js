9c68f73f05abb9f0cc01f5c807add3d0858123e8
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-11-26 04:22:20 -0300

    feat: category reducer

---
+++ src/redux/reducers/category/category.actions.js
@@ -0,0 +1,157 @@
+import axios from 'axios';
+import {
+  CATEGORY_LIST_REQUEST,
+  CATEGORY_LIST_SUCCESS,
+  CATEGORY_LIST_FAIL,
+  CATEGORY_DETAILS_REQUEST,
+  CATEGORY_DETAILS_SUCCESS,
+  CATEGORY_DETAILS_FAIL,
+  CATEGORY_DELETE_REQUEST,
+  CATEGORY_DELETE_SUCCESS,
+  CATEGORY_DELETE_FAIL,
+  CATEGORY_CREATE_REQUEST,
+  CATEGORY_CREATE_SUCCESS,
+  CATEGORY_CREATE_FAIL,
+  CATEGORY_UPDATE_REQUEST,
+  CATEGORY_UPDATE_SUCCESS,
+  CATEGORY_UPDATE_FAIL,
+} from './category.types';
+
+export const getCategories = () => async (dispatch) => {
+  try {
+    dispatch({ type: CATEGORY_LIST_REQUEST });
+    const { data } = await axios.get(`${process.env.REACT_APP_SERVER_DOMAIN}/categories/`);
+    dispatch({
+      type: CATEGORY_LIST_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: CATEGORY_LIST_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const getCategory = (slug) => async (dispatch) => {
+  try {
+    dispatch({ type: CATEGORY_DETAILS_REQUEST });
+    const { data } = await axios.get(
+      `${process.env.REACT_APP_SERVER_DOMAIN}/categories/${slug}`
+    );
+    dispatch({
+      type: CATEGORY_DETAILS_SUCCESS,
+      payload: data
+    });
+  } catch (error) {
+    dispatch({
+      type: CATEGORY_DETAILS_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const deleteCategory = (slug) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: CATEGORY_DELETE_REQUEST,
+    });
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: userInfo.token,
+      },
+    };
+    await axios.delete(`${process.env.REACT_APP_SERVER_DOMAIN}/categories/${slug}`, config);
+    dispatch({
+      type: CATEGORY_DELETE_SUCCESS,
+    });
+  } catch (error) {
+    dispatch({
+      type: CATEGORY_DELETE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const createCategory = (name) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: CATEGORY_CREATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        Authorization: userInfo.token,
+      },
+    };
+
+    const { data } = await axios.post(
+      `${process.env.REACT_APP_SERVER_DOMAIN}/categories/`, { name }, config
+    );
+
+    dispatch({
+      type: CATEGORY_CREATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
+    dispatch({
+      type: CATEGORY_CREATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
+
+export const updateCategory = (slug, name) => async (dispatch, getState) => {
+  try {
+    dispatch({
+      type: CATEGORY_UPDATE_REQUEST,
+    });
+
+    const {
+      userLogin: { userInfo },
+    } = getState();
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: userInfo.token,
+      },
+    };
+    const { data } = await axios.put(
+      `${process.env.REACT_APP_SERVER_DOMAIN}/categories/${slug}`,
+      { name },
+      config
+    );
+
+    dispatch({
+      type: CATEGORY_UPDATE_SUCCESS,
+      payload: data,
+    });
+  } catch (error) {
+    dispatch({
+      type: CATEGORY_UPDATE_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
