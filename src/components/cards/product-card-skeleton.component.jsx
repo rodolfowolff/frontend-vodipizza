6fb3d0cc644e23240467c11a906aff8a1ec062d0
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-12-15 14:49:18 -0300

    feat: product card  and skeleton components

--- src/components/cards/product-card-skeleton.component.jsx
+++ src/components/cards/product-card-skeleton.component.jsx
@@ -4,17 +4,17 @@ const ProductCardSkeleton = () => {
   return (
     <div className='space-y-4'>
       <div className='animate-pulse'>
-        <div className='pb-5/6 relative bg-gray-800 rounded-lg shadow-md'></div>
+        <div className='pb-5/6 relative bg-gray-500 rounded-lg shadow-md'></div>
         <div className='relative px-4 -mt-16'>
-          <div className='bg-gray-800 p-6 rounded-lg shadow-lg'>
+          <div className='bg-gray-300 p-6 rounded-lg shadow-lg'>
             <div className='flex-1 space-y-3 py-1'>
               <div className='space-y-1'>
-                <div className='h-4 bg-gray-800 rounded w-2/12'></div>
-                <div className='h-4 bg-gray-800 rounded w-5/6'></div>
+                <div className='h-4 bg-gray-500 rounded w-2/12'></div>
+                <div className='h-4 bg-gray-300 rounded w-5/6'></div>
               </div>
               <div className='space-y-2'>
-                <div className='h-4 bg-gray-800 rounded w-2/3'></div>
-                <div className='h-4 bg-gray-800 rounded w-2/3'></div>
+                <div className='h-4 bg-gray-500 rounded w-2/3'></div>
+                <div className='h-4 bg-gray-300 rounded w-2/3'></div>
               </div>
             </div>
           </div>
--- src/components/cards/product-card.component.jsx
+++ src/components/cards/product-card.component.jsx
@@ -1,19 +1,19 @@
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Highlight } from 'react-instantsearch-dom';
-
+import { currencyFormatter } from '../../utils/functions';
 import NoImage from '../../assets/images/no-product-image.png';
 
-const ProductCard = ({ product, ap }) => {
+const ProductCard = ({ product }) => {
   const { slug, title, images, category, price } = product;
   return (
     <div className='space-y-4 group rounded-xl hover:bg-transparent hover:shadow-2xl p-2'>
       <div className=''>
-          <Link to={`/product/${slug}`}>
+          <Link to={`/products/${slug}`}>
             <div className='pb-5/6 relative'>
               <img
                 className='h-full w-full object-cover absolute rounded-lg shadow-md'
-                src={images.length > 0 ? images[0]?.imageURL : NoImage}
+                src={images.length > 0 ? images[0].imageURL : NoImage}
                 alt={title}
               />
             </div>
@@ -21,13 +21,13 @@ const ProductCard = ({ product, ap }) => {
             <div className='bg-white p-6 rounded-lg shadow-lg'>
               <div className='flex items-baseline'>
                 <span className='inline-block bg-rose-200 text-rose-600 text-xs uppercase tracking-widest font-semibold px-2 rounded-sm'>
-                  {category?.name}
+                  {category.name}
                 </span>
               </div>
                 <h4 className='mt-1 font-semibold font-hind text-blue-gray-800 text-lg leading-snug truncate'>
-                  {ap ? <Highlight attribute='title' hit={product} /> : title }
+                  { title }
                 </h4>
-              <div className='mt-1 font-poppins text-blue-gray-800'>{price}</div>
+              <div className='mt-1 font-poppins text-blue-gray-800'>{currencyFormatter(price)}</div>
             </div>
           </div>
         </Link>
